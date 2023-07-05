4ade24f57ac284c946c0c42ddc7b634680d3a2cf
Anastasiya M <mega.gara11@gmail.com>
2023-07-05 20:05:44 +0700

    add src

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
 
-    public double calculate(double height, int weight) {
-        return weight/(height*height);
+    public double calculate(double height_meters, int weight_kg) {
+        return weight_kg/(height_meters*height_meters);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double height = 1.87;
-        int weight = 98;
-        double bmi1 = service.calculate(height, weight);
+        double height_meters = 1.87;
+        int weight_kg = 98;
+        double bmi1 = service.calculate(height_meters, weight_kg);
         int bmi = (int) bmi1;
-        System.out.println(bmi);
+        System.out.println(bmi + " - индекс массы тела");
 
     }
 }
